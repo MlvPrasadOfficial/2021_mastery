f176e39dbf9a6e6d43dfa1d6bf02624fcf725fc4
thehustlingmlv <iamprasad23@gmail.com>
2020-12-21 11:52:40 +0530

    dec 14
    Signed-off-by: thehustlingmlv <iamprasad23@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # my name is mlv prasad
 import pandas as pd
-mlv == pd.DataFrame({1:1,2:2})
+
+mlv = pd.DataFrame({1:1,2:2})
--- mlv.py
+++ mlv.py
@@ -1,2 +1,10 @@
 #2021 is the year
 # ai for me
+
+
+print("mlv")
+class Mlv :
+    def mlv :
+        print("mlv")
+    def munna :
+        print("ml")
